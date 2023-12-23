25d3e4e80fff1a085540fd854803ebc86bd2625a
NoahR <95318960+MinecraftXpert@users.noreply.github.com>
2023-12-23 11:49:22 -0500

    Add files via upload

---
+++ Buisness website clone website/scrolling.js
@@ -0,0 +1,8 @@
+const slide1 = document.querySelector("#slides__1");
+const slide2 = document.querySelector("#slides__2");
+const slide3 = document.querySelector("#slides__3");
+
+
+setInterval(() => {
+    
+}, 5000);
